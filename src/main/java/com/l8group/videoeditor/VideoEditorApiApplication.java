9ab8c82b31c212cc0752deb0ef5217b2376869f3
Michael Henrique Cervantes da Silva <michael.cervantes@l8group.net>
2024-12-31 00:15:17 -0300

    chore: Initial setup with Spring Boot and basic dependencies

---
+++ src/main/java/com/l8group/videoeditor/VideoEditorApiApplication.java
@@ -0,0 +1,13 @@
+package com.l8group.videoeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoEditorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoEditorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l8group/videoeditor/VideoEditorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l8group.videoeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoEditorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
